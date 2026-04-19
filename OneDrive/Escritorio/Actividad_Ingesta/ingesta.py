b008e7e27d1a9809b7ab59d95207bd2cbdcd385b
EzoGV <en.gabrielli@duocuc.cl>
2026-04-19 17:58:33 -0400

    feat: ingesta nivel basico

---
+++ OneDrive/Escritorio/Actividad_Ingesta/ingesta.py
@@ -0,0 +1,13 @@
+import shutil
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+origen = "origen/ventas.csv"
+destino = "data/raw/ventas.csv"
+
+try:
+    shutil.copy(origen, destino)
+    logging.info("Archivo copiado correctamente")
+except Exception as e:
+    logging.error(f"Error en la ingesta: {e}")
