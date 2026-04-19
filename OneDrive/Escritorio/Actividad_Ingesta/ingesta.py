126ef3369b812337491e7186c20cd5282b0e559b
EzoGV <en.gabrielli@duocuc.cl>
2026-04-19 18:09:15 -0400

    Nivel Basico

--- OneDrive/Escritorio/Actividad_Ingesta/ingesta.py
+++ OneDrive/Escritorio/Actividad_Ingesta/ingesta.py
@@ -1,24 +1,14 @@
 import shutil
 import logging
-import os
 
-logging.basicConfig(level=logging.INFO)
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 origen = "origen/ventas.csv"
 destino = "data/raw/ventas.csv"
 
 try:
-    if os.path.exists(origen):
-        os.makedirs("data/raw", exist_ok=True)
-        shutil.copy(origen, destino)
-        logging.info("Archivo copiado correctamente")
-        
-        with open(destino, 'r') as f:
-            registros = sum(1 for line in f) - 1
-        logging.info(f"Cantidad de registros procesados: {registros}")
-        
-    else:
-        logging.error("Error: El archivo de ventas.csv no se encuentra en la carpeta origen.")
-
+    shutil.copy(origen, destino)
+    logging.info("Archivo copiado correctamente")
 except Exception as e:
     logging.error(f"Error en la ingesta: {e}")
