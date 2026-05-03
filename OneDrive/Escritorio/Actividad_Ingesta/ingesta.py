0506c3179d40cd384ea822cdc438a913fa1617a8
EzoGV <en.gabrielli@duocuc.cl>
2026-05-03 02:07:56 -0400

    Update error ingesta.py
    Se soluciona error con commits.

--- OneDrive/Escritorio/Actividad_Ingesta/ingesta.py
+++ OneDrive/Escritorio/Actividad_Ingesta/ingesta.py
@@ -21,21 +21,22 @@ def generar_respaldo_raw():
     
     try:
         logging.info("---INGESTA CAPA RAW ---")
-        
+
         #Aseguramos que la carpeta data/raw exista
         os.makedirs("data/raw", exist_ok=True)
-        
+
         if not os.path.exists(origen):
             logging.error(f"ERROR: No se encontro el archivo original en {origen}. Asegurate de haber generado los 1000 registros.")
             return
 
         #Procesamos el archivo de 1000 registros
+
         with open(origen, 'r', encoding='utf-8') as f_in, \
              open(destino_raw, 'w', newline='', encoding='utf-8') as f_out:
-            
+
             lector = csv.reader(f_in)
             escritor = csv.writer(f_out)
-            
+
             #Leemos la cabecera (id_venta, fecha_venta, producto, cantidad, precio_total, rut)
             try:
                 cabecera = next(lector)
@@ -45,17 +46,17 @@ def generar_respaldo_raw():
 
             #Escribimos la cabecera + la columna de control interna
             escritor.writerow(cabecera + ["fecha_ingesta"])
-            
+
             #Marcamos el momento exacto en que los datos entran al pipeline
             timestamp_ingesta = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             conteo_registros = 0
-            
+
             for fila in lector:
                 # erificamos que la fila no esté vacía
                 if any(fila):
                     escritor.writerow(fila + [timestamp_ingesta])
                     conteo_registros += 1
-                
+
         logging.info(f"EXITO: Se procesaron {conteo_registros} registros correctamente.")
         logging.info(f"Archivo Raw generado")
 
